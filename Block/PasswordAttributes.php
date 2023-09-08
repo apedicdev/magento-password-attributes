0d4238ca7ad38977d690c88effb4cb3513487334
apedik <apedicini@sqli.com>
2023-09-08 22:47:46 +0100

    Mftf tests added

--- Block/PasswordAttributes.php
+++ Block/PasswordAttributes.php
@@ -13,10 +13,10 @@ readonly class PasswordAttributes implements ArgumentInterface
     public const UPPERCASE_DIGITS_REQUIREMENTS = 2;
     public const LOWERCASE_UPPERCASE_DIGITS_REQUIREMENTS = 3;
     public const LOWERCASE_UPPERCASE_DIGITS_SPECIAL_CHARS_REQUIREMENTS = 4;
-    private const ONLY_DIGITS_PATTERN = '^(?=.*\d)[\d]{%s,}$';
-    private const UPPERCASE_DIGITS_PATTERN = '^(?=.*[A-Z])(?=.*\d)[A-Z\d]{%s,}$';
-    private const UPPERCASE_LOWERCASE_DIGITS_PATTERN = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{%s,}$';
-    private const UPPERCASE_LOWERCASE_DIGITS_SPECIAL_CHARS_PATTERN = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{%s,}$';
+    private const ONLY_DIGITS_PATTERN = '^(?=.*\d).{%s,}$';
+    private const UPPERCASE_DIGITS_PATTERN = '^(?=.*[A-Z])(?=.*\d).{%s,}$';
+    private const UPPERCASE_LOWERCASE_DIGITS_PATTERN = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{%s,}$';
+    private const UPPERCASE_LOWERCASE_DIGITS_SPECIAL_CHARS_PATTERN = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{%s,}$';
 
     private const REQUIREMENTS = [
         self::ONLY_DIGITS_REQUIREMENTS => 'required: digit',
